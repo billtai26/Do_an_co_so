fd57c4ae680c4780a6043c789f8dcd391e364202
billtai26 <anhtaipxuan@gmail.com>
2025-04-01 09:47:26 +0700

    Create User Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Board from '~/pages/Boards/_id.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import UserProfile from '~/components/UserProfile'
 
 function App() {
   return (
-    <>
-      <Board />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Board />} />
+        <Route path="/profile" element={<UserProfile />} />
+      </Routes>
+    </Router>
   )
 }
 
