64fe936e1723de84d06e4d00b337a5c27be87029
billtai26 <anhtaipxuan@gmail.com>
2025-04-12 20:33:22 +0700

    Drag Drop Card P1

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '~/App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
-import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 import { getTheme } from '~/theme' // Sửa thành import named export
 
 // Tạo theme mặc định (light mode)
