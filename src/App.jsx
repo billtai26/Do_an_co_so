64d82d0573be547cbacf87b4fe59a319b267cb8b
billtai26 <anhtaipxuan@gmail.com>
2025-04-14 10:14:48 +0700

    Fix some bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Board from '~/pages/Boards/_id.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import UserProfile from '~/components/UserProfile'
 import TaskManager from '~/components/TaskManager'
-import CommentsSection from '~/components/CommentsSection'
+import CommentsSection from '~/components/CommentsSection';
 
 
 function App() {
