15903c4424d5b96110be502eade1fac1e62b98a4
billtai26 <anhtaipxuan@gmail.com>
2025-04-21 00:29:00 +0700

    Custom Libs

---
+++ src/customHooks/index.js
@@ -0,0 +1,4 @@
+/**
+ * Youtube: Trungquandev - Một lập trình viên
+ * Create by trungquandev.com's author on Nov 12,2023
+ */
--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -6,8 +6,8 @@ import { mapOrder } from '~/utils/sorts'
 import {
   DndContext,
   // PointerSensor,
-  MouseSensor,
-  TouchSensor,
+  // MouseSensor,
+  // TouchSensor,
   useSensor,
   useSensors,
   DragOverlay,
@@ -18,6 +18,8 @@ import {
   // rectIntersection,
   getFirstCollision
 } from '@dnd-kit/core'
+import { MouseSensor, TouchSensor } from '~/customLibraries/DndKitSensors'
+
 import { arrayMove } from '@dnd-kit/sortable'
 import { useEffect, useState, useCallback, useRef } from 'react'
 import { cloneDeep, isEmpty } from 'lodash'
