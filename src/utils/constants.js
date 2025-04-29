6d0a99f3ccff6df46ddeefa5e27e5458df5d516d
billtai26 <anhtaipxuan@gmail.com>
2025-04-29 21:28:31 +0700

    Fix BUILD_MODE dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,15 @@
-export const API_ROOT = 'http://localhost:8017'
-// export const API_ROOT = 'https://do-an-co-so-api.onrender.com'
+let apiRoot = ''
+
+console.log('import.meta.env: ', import.meta.env)
+console.log('process.env: ', process.env)
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://do-an-co-so-api.onrender.com'
+}
+
+console.log('🚀 ~ apiRoot:', apiRoot)
+
+export const API_ROOT = apiRoot
