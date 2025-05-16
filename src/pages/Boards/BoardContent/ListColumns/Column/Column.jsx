5732ff7ce1df0f610b7cde236b226a6162219016
billtai26 <anhtaipxuan@gmail.com>
2025-05-16 23:44:25 +0700

    Lesson 05: Create APIs Register

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -291,6 +291,7 @@ function Column({ column }) {
               />
               <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
                 <Button
+                  className="interceptor-loading"
                   onClick={addNewCard}
                   variant="contained" color="success" size="small"
                   sx={{
