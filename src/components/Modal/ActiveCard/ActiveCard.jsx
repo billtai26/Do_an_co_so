04b33fc5b9a7c2095bdd8d25bcc9243eb10943f3
billtai26 <anhtaipxuan@gmail.com>
2025-05-27 00:07:21 +0700

    Lesson 19: Tính năng Card Members

--- src/components/Modal/ActiveCard/ActiveCard.jsx
+++ src/components/Modal/ActiveCard/ActiveCard.jsx
@@ -121,6 +121,10 @@ function ActiveCard() {
     await callApiUpdateCard({ commentToAdd })
   }
 
+  const onUpdateCardMembers = () => {
+    
+  }
+
   return (
     <Modal
       disableScrollLock
@@ -176,7 +180,10 @@ function ActiveCard() {
               <Typography sx={{ fontWeight: '600', color: 'primary.main', mb: 1 }}>Members</Typography>
 
               {/* Feature 02: Xử lý các thành viên của Card */}
-              <CardUserGroup />
+              <CardUserGroup
+                cardMemberIds={activeCard?.memberIds}
+                onUpdateCardMembers={onUpdateCardMembers}
+              />
             </Box>
 
             <Box sx={{ mb: 3 }}>
